45bd8f9d757354cae3547277f25ec550c1c069ee
Mubashir <mubashirseo100@gmail.com>
2024-04-03 03:22:48 -0700

    todos

---
+++ app.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todosquestions = await inquirer.prompt([
+        {
+            name: "todos",
+            type: "input",
+            message: "what would you like to add in your todos?"
+        },
+        {
+            name: "todosq2",
+            type: "confirm",
+            message: "would you like to add more in your todos?",
+            default: "true "
+        }
+    ]);
+    todos.push(todosquestions.todos);
+    console.log(todos);
+    condition = todosquestions.todosq2;
+}
